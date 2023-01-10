dec0a5061d4e9a009d7fa939896f209d18b58138
AniketWaJe <aniket.waje@trunexa.com>
2023-01-10 14:42:23 +0530

    board/Trucrux/imx8mq_trucrux: spl.c : PMIC Optimized
    Optimized PMIC to control heating of SOM.

--- board/Trucrux/imx8mq_trucrux/spl.c
+++ board/Trucrux/imx8mq_trucrux/spl.c
@@ -249,26 +249,35 @@ int power_init_board(void)
 		int ret;
 		ret = power_bd71837_init(PMIC_I2C_BUS);
 		if (ret)
-				printf("power init failed");
+			printf("power init failed\n");
+		else
+			printf("PMIC: BD71837 Found\n");
 
 		p = pmic_get("BD71837");
 		pmic_probe(p);
 
+		/* decrease RESET key long push time from the default 10s to 10ms */
 		pmic_reg_write(p, BD71837_PWRONCONFIG1, 0x0);
 
+		/* unlock the PMIC regs */
 		pmic_reg_write(p, BD71837_REGLOCK, 0x1);
 
-		pmic_reg_write(p, BD71837_BUCK1_VOLT_RUN, 0x14);  //0x0f);
+		/* increase VDD_SOC to typical value 0.85v before first DRAM access */
+		pmic_reg_write(p, BD71837_BUCK1_VOLT_RUN, 0x0f);
 
-		pmic_reg_write(p, BD71837_BUCK5_VOLT, 0x03); //0x83);
+		/* increase VDD_DRAM to 0.975v for 3Ghz DDR */
+		pmic_reg_write(p, BD71837_BUCK5_VOLT, 0x83);
 
-		pmic_reg_write(p, BD71837_LDO5_VOLT, 0x00); //0xc0);
-
+		/* Enabled NVCC_DRAM */
 		pmic_reg_write(p, BD71837_BUCK8_CTRL, 0x03);
 
+		/* Enable LDO5 - PHY supply to 1.8V */
+		pmic_reg_write(p, BD71837_LDO5_VOLT, 0xc0);
+
+		/* lock the PMIC regs */
 		pmic_reg_write(p, BD71837_REGLOCK, 0x11);
 
-	return 0;
+		return 0;
 
 }
 #endif
